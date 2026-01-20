20c505c3c12406cc5edf115890387f88a162df25
letscodingsowecanplay <nanatyourcircuss@gmail.com>
2026-01-20 14:02:41 +0700

    commit appservice

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this-> app->environment('productions')) {
+            \URL:::forceSchema('https');
+        }
         Paginator::useBootstrapFive();
 
         // Cek user login dan role siswa
